a4936ceeba7be1fe17b138a5d7005c1a346496d8
Victo <victorcarmona9035@hotmail.com>
2024-05-05 19:58:15 +0100

    Creacion de archivos y directorios

---
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './defer-views.component.html',
+  styles: ``
+})
+export class DeferViewsComponent {
+
+}
