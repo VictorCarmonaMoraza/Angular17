311e2056d4c823926977d40825de118e01be6999
Victo <victorcarmona9035@hotmail.com>
2024-05-11 08:08:07 +0100

    Configuracion del dashboard - Tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,15 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
-  standalone  : true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.css']
 })
-export class SidemenuComponent{
+export class SidemenuComponent {
 
 
+  constructor() {
+    const dashboardRoutes = routes
+      .map(route => route.children ?? [])
+      .flat()
+      //muestra solo las rutas que no tienen el path vacio
+      .filter(route => route && route.path)
+      //Excluir la ruita user/:id;
+      .filter(route => !route.path?.includes(':'))
 
+    console.log(dashboardRoutes)
+  }
 }
